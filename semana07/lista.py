80726d3c685e55296e3ff4d9d8a10e6e4093f9c4
Thiago Trannin <51510921+thinety@users.noreply.github.com>
2021-06-15 15:21:19 -0300

    Lista semana 07

--- semana07/lista.py
+++ semana07/lista.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.set_printoptions(precision=6, floatmode='maxprec_equal', suppress=True)
+np.set_printoptions(precision=4, floatmode='maxprec_equal', suppress=True)
 
 
 def gauss_seidel(M, X, max_i, max_err):
@@ -86,3 +86,45 @@ relaxacao(M, X, 10, 0.1)
 
 
 # Lista
+
+# Questão 1
+
+# M = np.array([[16.1,   2.8,  2.7,  9.9,  -26.5],
+#               [ 1.0,   8.2, -4.9, 15.4,  236.0],
+#               [ 8.7, -19.1, -3.2, -6.2, -269.3],
+#               [ 8.6,   9.0, 23.8, -6.0, -187.3]], np.float64)
+M = np.array([[16.1,   2.8,  2.7,  9.9,  -26.5],
+              [ 8.7, -19.1, -3.2, -6.2, -269.3],
+              [ 8.6,   9.0, 23.8, -6.0, -187.3],
+              [ 1.0,   8.2, -4.9, 15.4,  236.0]], np.float64)
+X = np.array([-8.1, 8.2, -5.6, 9.7], np.float64)
+
+gauss_seidel(M, X, 5, 0.001)
+
+# Questão 4
+
+M = np.array([[14.1, -2.8,  9.5, 135.1],
+              [-2.6, -4.6,  0.1,  15.4],
+              [ 2.1,  6.0, -9.0, -32.0]], np.float64)
+X = np.array([7.9, -7.8, 0.2], np.float64)
+
+gauss_seidel(M, X, 5, 0.001)
+
+# Questão 2
+
+M = np.array([[-0.5, -2.3,  1.7,  8.0,  73.4],
+              [ 1.8, -2.6, -5.3,  5.9, -32.1],
+              [-7.7, -3.9, -7.0,  3.7,   7.3],
+              [-3.7,  8.1,  6.7, -1.5, 123.8]], np.float64)
+X = np.array([-8.4, 4.9, 9.7, 8], np.float64)
+
+relaxacao(M, X, 6, 0.01)
+
+# Questão 3
+
+M = np.array([[-2.3, -5.4, -9.8, -83.7],
+              [-2.7,  6.7, -5.5, -74.8],
+              [ 6.8,  1.6,  0.6,  56.2]], np.float64)
+X = np.array([8, -1.7, 7.6], np.float64)
+
+relaxacao(M, X, 6, 0.01)
